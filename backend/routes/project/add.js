94d75bf37ef828d3771c030d2db04ddc8063751a
Loujaina <loujaina96@hotmail.com>
2019-12-14 15:06:54 +0200

    Project images saved on google drive

--- backend/routes/project/add.js
+++ backend/routes/project/add.js
@@ -1,17 +1,18 @@
 const Colaber = require('../../models/Colaber');
 const Project = require('../../models/Project');
 const { validationResult } = require('express-validator');
+const drive = require('../../services/drive');//google drive
+
 
 
 module.exports = async (req, res) => {
 
     const errors = validationResult(req);
-    console.log(errors);
 
     //if there are missing fields
     if (!errors.isEmpty()) {
         console.log('errors');
-        return res.status(400).json({ message: errors.errors[0].msg});
+        return res.status(400).json({ message: errors.errors[0].msg });
     }
 
     console.log('new Project attempt');
@@ -19,25 +20,55 @@ module.exports = async (req, res) => {
     //pull from request
     const { name, description } = req.body;
     console.log(req.body);
-  
+    //pull images
+    var files;
+    try {
+        files = req.files;
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Server Error' })
+    }
 
     try {
         //create new project and save to database
-        project = new Project ({
-            name, 
+        project = new Project({
+            name,
             description,
             creator: req.user.id
         });
         await project.save();
+
+        //save images to google drive
+        var urls;
+        if (files && files.length > 0) {
+             urls = await drive.uploadImages(project._id, files);
+            if (urls == null)
+               {
+                //remove from DB
+               await Project.remove({ _id: project.id })
+                return res.status(500).json({ message: 'Server Error' });
+
+               } 
+
+               //push images urls to the DB->
+               
+               await Project.updateOne(
+                { "_id": project.id }, 
+                { "$push": { "images": { "$each": urls } } },
+               ) 
+
+        }
         
+
+
         //get user and add project to the user's project list
-        let user = await Colaber.findOne({_id: req.user.id}).select('-password'); 
-        if (user){
+        let user = await Colaber.findOne({ _id: req.user.id }).select('-password');
+        if (user) {
             user.projects.unshift(project.id);
         }
 
         await user.save();
-        res.json(project)
+        res.json(await Project.findOne({_id:project.id}));
 
     } catch (error) {
         console.log(error.message);
--- backend/routes/project/index.js
+++ backend/routes/project/index.js
@@ -1,5 +1,7 @@
 const project = require('express').Router();
-
+const multer = require('multer');
+const upload = multer(); //multer handles images because json doesn't support binary files
+const drive=require('../../services/drive');
 
 //checks req.body for missing fields
 const { check } = require('express-validator');
@@ -11,12 +13,13 @@ const jwt = require('jsonwebtoken');
 //@route GET    project/view
 //@description  view project page
 //@access       public 
-project.get('/view/:proj_id',require('./view'));
+project.get('/view/:proj_id', require('./view'));
 
 //@route POST   project/add
 //@description  add project to user 
 //@access       auth needed 
 project.post('/add', [
+    upload.array('photos',10),//max number of pics->10
     check('name', 'Please enter project name').not().isEmpty(),
     auth
 ], require('./add'));
@@ -27,4 +30,16 @@ project.post('/add', [
 //@access       auth needed 
 project.delete('/delete/:proj_id', auth, require('./delete'));
 
-module.exports=project;
+
+//DEVELOPMENT ROUTE -> drive cleanup
+project.delete('/folder',async(req,res)=>{
+    if(await drive.driveCleanUp()==true){
+        res.send('Deleted successfully');
+    }
+    else{
+        res.send('No folders were found');
+    }
+
+})
+
+module.exports = project;
